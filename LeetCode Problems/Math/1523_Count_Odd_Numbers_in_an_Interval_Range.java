ae5898aae11a4e82c76c56d14bf92bc1cda7e067
Hammad Babar Ali <hammadbabar0326@gmail.com>
2026-08-20 17:30:53 +0500

    Delete LeetCode Problems directory

--- LeetCode Problems/Math/1523_Count_Odd_Numbers_in_an_Interval_Range.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public int countOdds(int low, int high) {
-        int oddTillHigh = (high + 1) / 2;
-        int oddBeforeLow = low / 2;
-        int oddRange = oddTillHigh - oddBeforeLow;
-        return oddRange;
-    }
-}
